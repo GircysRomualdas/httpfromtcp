0dd0573a8a4cb436e1e75125c678fdf3dfb52042
romas <r2udis@gmail.com>
2025-08-28 01:18:27 +0300

    add read file

--- main.go
+++ main.go
@@ -2,8 +2,28 @@ package main
 
 import (
 	"fmt"
+	"io"
+	"os"
 )
 
 func main() {
-	fmt.Println("I hope I get the job!")
+	file, err := os.Open("messages.txt")
+	if err != nil {
+		fmt.Println("Error opening file:", err)
+		return
+	}
+	defer file.Close()
+
+	for {
+		readBytes := make([]byte, 8)
+		_, err = file.Read(readBytes)
+		if err != nil {
+			if err == io.EOF {
+				break
+			}
+			fmt.Println("Error reading file:", err)
+			return
+		}
+		fmt.Printf("read: %s\n", readBytes)
+	}
 }
