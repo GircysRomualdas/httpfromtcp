5adc33e50f9a3c7fc9bebbe83d45e8c9f10c67cd
romas <r2udis@gmail.com>
2025-08-31 23:47:53 +0300

    add handler

--- cmd/httpserver/main.go
+++ cmd/httpserver/main.go
@@ -1,18 +1,36 @@
 package main
 
 import (
+	"io"
 	"log"
 	"os"
 	"os/signal"
 	"syscall"
 
+	"github.com/GircysRomualdas/httpfromtcp/internal/request"
+	"github.com/GircysRomualdas/httpfromtcp/internal/response"
 	"github.com/GircysRomualdas/httpfromtcp/internal/server"
 )
 
 const port = 42069
 
 func main() {
-	server, err := server.Serve(port)
+	server, err := server.Serve(port, func(w io.Writer, req *request.Request) *server.HandlerError {
+		if req.RequestLine.RequestTarget == "/yourproblem" {
+			return &server.HandlerError{
+				StatusCode: response.BadRequest,
+				Message:    "Your problem is not my problem\n",
+			}
+		} else if req.RequestLine.RequestTarget == "/myproblem" {
+			return &server.HandlerError{
+				StatusCode: response.InternalServerError,
+				Message:    "Woopsie, my bad\n",
+			}
+		} else {
+			w.Write([]byte("All good, frfr\n"))
+		}
+		return nil
+	})
 	if err != nil {
 		log.Fatalf("Error starting server: %v", err)
 	}
--- internal/server/server.go
+++ internal/server/server.go
@@ -1,31 +1,48 @@
 package server
 
 import (
+	"bytes"
 	"fmt"
+	"io"
+	"log"
 	"net"
 	"sync/atomic"
 
+	"github.com/GircysRomualdas/httpfromtcp/internal/request"
 	"github.com/GircysRomualdas/httpfromtcp/internal/response"
 )
 
 type Server struct {
 	listener net.Listener
 	closed   atomic.Bool
+	handler  Handler
 }
 
-func Serve(port int) (*Server, error) {
+type HandlerError struct {
+	StatusCode response.StatusCode
+	Message    string
+}
+
+type Handler func(w io.Writer, req *request.Request) *HandlerError
+
+func (he *HandlerError) Write(w io.Writer) {
+	response.WriteStatusLine(w, he.StatusCode)
+	headers := response.GetDefaultHeaders(len(he.Message))
+	response.WriteHeaders(w, headers)
+	w.Write([]byte(he.Message))
+}
+
+func Serve(port int, handler Handler) (*Server, error) {
 	listener, err := net.Listen("tcp", fmt.Sprintf(":%d", port))
 	if err != nil {
-		return nil, fmt.Errorf("failed to listen on port %d: %w", port, err)
+		return nil, err
 	}
-	s := &Server{listener: listener}
+	s := &Server{
+		handler:  handler,
+		listener: listener,
+	}
 	go s.listen()
 	return s, nil
-}
-
-func (s *Server) Close() error {
-	s.closed.Store(true)
-	return s.listener.Close()
 }
 
 func (s *Server) listen() {
@@ -35,25 +52,38 @@ func (s *Server) listen() {
 			if s.closed.Load() {
 				return
 			}
-			fmt.Printf("failed to accept connection: %v\n", err)
+			log.Printf("Error accepting connection: %v", err)
 			continue
 		}
 		go s.handle(conn)
 	}
 }
 
+func (s *Server) Close() error {
+	s.closed.Store(true)
+	return s.listener.Close()
+}
+
 func (s *Server) handle(conn net.Conn) {
 	defer conn.Close()
-
-	err := response.WriteStatusLine(conn, response.OK)
+	req, err := request.RequestFromReader(conn)
 	if err != nil {
-		fmt.Printf("failed to write status line: %v\n", err)
+		hErr := &HandlerError{
+			StatusCode: response.BadRequest,
+			Message:    err.Error(),
+		}
+		hErr.Write(conn)
 		return
 	}
-	headers := response.GetDefaultHeaders(0)
-	err = response.WriteHeaders(conn, headers)
-	if err != nil {
-		fmt.Printf("failed to write headers: %v\n", err)
+	buf := bytes.NewBuffer([]byte{})
+	hErr := s.handler(buf, req)
+	if hErr != nil {
+		hErr.Write(conn)
 		return
 	}
+	b := buf.Bytes()
+	response.WriteStatusLine(conn, response.OK)
+	headers := response.GetDefaultHeaders(len(b))
+	response.WriteHeaders(conn, headers)
+	conn.Write(b)
 }
